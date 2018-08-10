d4b27da21e58a4e180392739758c5f654d28f4a9
Jean-Marc Prieur <jmprieur@microsoft.com>
2018-08-10 10:23:31 +0200

    Improving the token validation

--- TodoListService/Extensions/AzureAdAuthenticationBuilderExtensions.cs
+++ TodoListService/Extensions/AzureAdAuthenticationBuilderExtensions.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using System;
+using System.Threading.Tasks;
 using TodoListService.Extensions;
 
 namespace Microsoft.AspNetCore.Authentication
@@ -20,7 +21,7 @@ namespace Microsoft.AspNetCore.Authentication
             return builder;
         }
 
-        private class ConfigureAzureOptions: IConfigureNamedOptions<JwtBearerOptions>
+        private class ConfigureAzureOptions : IConfigureNamedOptions<JwtBearerOptions>
         {
             private readonly AzureAdOptions _azureOptions;
 
@@ -32,64 +33,34 @@ namespace Microsoft.AspNetCore.Authentication
 
             public void Configure(string name, JwtBearerOptions options)
             {
-                options.Audience = _azureOptions.ClientId;
                 options.Authority = $"{_azureOptions.Instance}{_azureOptions.TenantId}/v2.0/";
 
+                // Specific validation. 
+                //- Audiance: this Web API accepts a token for its client ID or api://{clientID}
+                //- Issuer: during the portal private preview, it accepts login.onmicrosoft.com AAD v1.0 and v2.0 and sts.windows.net
                 TokenValidationParameters tokenValidationParameter = options.TokenValidationParameters;
+                tokenValidationParameter.ValidAudiences = new string[] { _azureOptions.ClientId, $"api://{_azureOptions.ClientId}" };
+                tokenValidationParameter.ValidIssuers = new string[]
+                {
+                    $"{_azureOptions.Instance}{_azureOptions.TenantId}/",
+                    $"{_azureOptions.Instance}{_azureOptions.TenantId}/v2.0/",
+                    $"https://sts.windows.net/{_azureOptions.TenantId}/"
+                };
 
-                // Instead of using the default validation (validating against a single tenant, as we do in line of business apps),
-                // we inject our own multitenant validation logic (which even accepts both V1 and V2 tokens)
-                tokenValidationParameter.ValidateIssuer = true;
-
-                // If you want to use the V2 endpoint (that is authority = $"{_azureOptions.Instance}common/v2.0/") 
-                // you'd also want to validate which tenants your Web API accept
-                // in that case you'd have to implement a IssuerValidator and uncomment the following line.
-                tokenValidationParameter.IssuerValidator = ValidateIssuer;
-
-                // This is needed so that the Web API can then call another web API with the on-behalf-of flow
-                // in the name of the user
+                // This is needed so that the Web API can then call another web API with the on-behalf-of flow in the name of the user
                 tokenValidationParameter.SaveSigninToken = true;
+
+                // In case of authentication failed, give us a chance to understand what was invalid, by looking at the exception.
+                options.Events = new JwtBearerEvents();
+                options.Events.OnAuthenticationFailed = AuthenticationFailed;
             }
 
-            /// <summary>
-            /// Validate the issuer so that both Azure AD V1, and Azure AD V2 endpoints are accepted, and for any
-            /// tenant
-            /// </summary>
-            /// <param name="issuer">Issuer to validate (will be tenanted)</param>
-            /// <param name="securityToken">Received Security Token</param>
-            /// <param name="validationParameters">Token Validation parameters</param>
-            /// <remarks>The issuer is considered as valid if it has the same http scheme and authority as the
-            /// authority from the configuration file, has a tenant Id, and optionally v2.0 (this web api
-            /// accepts both V1 and V2 tokens)</remarks>
-            /// <returns>The <c>issuer</c> if it's valid, or otherwise <c>null</c></returns>
-            private string ValidateIssuer(string issuer, SecurityToken securityToken, TokenValidationParameters validationParameters)
+            private async Task AuthenticationFailed(AuthenticationFailedContext context)
             {
-                Uri uri = new Uri(issuer);
-                Uri authorityUri = new Uri(_azureOptions.Instance);
-                string[] parts = uri.AbsolutePath.Split('/');
-                if (parts.Length >= 2)
-                {
-                    Guid tenantId;
-                    if (uri.Scheme != authorityUri.Scheme || uri.Authority != authorityUri.Authority)
-                    {
-                        throw new SecurityTokenInvalidIssuerException("Issuer has wrong authority");
-                    }
-                    if (!Guid.TryParse(parts[1], out tenantId))
-                    {
-                        throw new SecurityTokenInvalidIssuerException("Cannot find the tenant GUID for the issuer");
-                    }
-                    if (parts.Length> 2 && parts[2] != "v2.0")
-                    {
-                        throw new SecurityTokenInvalidIssuerException("Only accepted protocol versions are AAD v1.0 or V2.0");
-                    }
-                    return issuer;
-                }
-                else
-                {
-                    throw new SecurityTokenInvalidIssuerException("Unknown issuer");
-                }
+                string message = context.Exception.Message;
+                throw context.Exception;
             }
-
+  
             public void Configure(JwtBearerOptions options)
             {
                 Configure(Options.DefaultName, options);
